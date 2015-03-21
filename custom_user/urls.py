aaf7e68e5840af3abe83cbff802b97927f7b9853
michaelcrubenstein <michaelcrubenstein@gmail.com>
2015-03-20 23:37:17 -0600

    Link url reference to dico/index

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'custom_user.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('dico.urls')),
     url(r'^dico/', include('dico.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )

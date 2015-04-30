f6fdc374935e028b499c7a21581b6a63c42b014c
michaelcrubenstein <michaelcrubenstein@gmail.com>
2015-04-30 09:00:12 -0400

    Add spinner when showing a map

--- public/static/dico/js/mapus.js
+++ public/static/dico/js/mapus.js
@@ -118,6 +118,27 @@ function mapdatapoints(mapFrameID, constituentAreaLabelID, constituentTableBodyI
 function show_map(mapFrameID, districtLabel, districtSpan, usjsonfile, congressjsonfile, showDataFunc, newScope) {
 	var circleData = [];
 
+	var opts = {
+	  lines: 13, // The number of lines to draw
+	  length: 20, // The length of each line
+	  width: 10, // The line thickness
+	  radius: 30, // The radius of the inner circle
+	  corners: 1, // Corner roundness (0..1)
+	  rotate: 0, // The rotation offset
+	  direction: 1, // 1: clockwise, -1: counterclockwise
+	  color: '#000', // #rgb or #rrggbb or array of colors
+	  speed: 1, // Rounds per second
+	  trail: 60, // Afterglow percentage
+	  shadow: false, // Whether to render a shadow
+	  hwaccel: false, // Whether to use hardware acceleration
+	  className: 'spinner', // The CSS class to assign to the spinner
+	  zIndex: 2e9, // The z-index (defaults to 2000000000)
+	  top: '50%', // Top position relative to parent
+	  left: '50%' // Left position relative to parent
+	};
+	var target = document.getElementById(mapFrameID.substring(1));
+	var spinner = new Spinner(opts).spin(target);
+
 	var svg = d3.select(mapFrameID).select("svg");
 	
 	function mouseoverstate() {
@@ -243,6 +264,8 @@ function show_map(mapFrameID, districtLabel, districtSpan, usjsonfile, congressj
 				.attr("r", function(d) { return d.r; })
 				.attr("id", function(d) { return d.id; })
 				.call(showDataFunc);
+		
+		spinner.stop();
 	}
 
 	// Set the label for the object when mousing over.

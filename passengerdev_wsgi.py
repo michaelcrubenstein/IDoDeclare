85015d0d1f676106c116cd5b619f776de1f9bcda
michaelcrubenstein <michaelcrubenstein@gmail.com>
2015-06-13 22:46:46 -0400

    Set up development version of passenger_wsgi

--- passengerdev_wsgi.py
+++ passengerdev_wsgi.py
@@ -3,10 +3,14 @@ import traceback
 import datetime
 
 cwd = os.getcwd()
-sys.path.insert(0,cwd+'/env/bin')
-sys.path.insert(0,cwd+'/env/lib/python3.4/site-packages/django')
-sys.path.insert(0,cwd+'/env/lib/python3.4/site-packages')
-sys.path.insert(0,cwd+'/env/lib/python3.4')
+homed = os.environ['HOME']
+bindirectory = homed + '/opt/python-3.4.2/bin'
+libdirectory = homed + '/opt/python-3.4.2/lib'
+pythondirectory = libdirectory + '/python3.4'
+sys.path.insert(0,bindirectory)
+sys.path.insert(0,pythondirectory+'/site-packages/django')
+sys.path.insert(0,pythondirectory+'/site-packages')
+sys.path.insert(0,pythondirectory)
 sys.path.insert(0,cwd + '/dico')  #You must add your project here
 sys.path.insert(0,cwd + '/custom_user')  #You must add your project here
 sys.path.append(cwd)
@@ -27,7 +31,7 @@ with open(cwd + '/passengerwsgi.log', 'a') as log:
         log.flush()
 
 #Switch to new python
-if sys.version < "3.4.2": os.execl(cwd+"/env/bin/python3.4", cwd+"/env/bin/python3.4", *sys.argv)
+if sys.version < "3.4.2": os.execl(bindirectory+"/python3.4", bindirectory+"/python3.4", *sys.argv)
 
 # os.environ['DJANGO_SETTINGS_MODULE'] = "custom_user.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "custom_user.settings")
@@ -36,6 +40,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "custom_user.settings")
 os.environ.setdefault("SUNLIGHT_API_KEY", 'f9defbcc52934ab38960bd1415ad3906')
 
 try:
+    import mysql.connector.django
     from django.core.wsgi import get_wsgi_application
     application = get_wsgi_application()
 
